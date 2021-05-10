aa8a5d22640085b36331639487348a8bbc95d3eb
James-Rouse <woootlooops@gmail.com>
2021-05-10 11:43:20 -0400

    Update guessing_game.py

--- guessing_game.py
+++ guessing_game.py
@@ -3,12 +3,23 @@ import random
 print("Welcome to the Number Guessing Game!")
 answer_number = random.randrange(10)
 
-try:
-    player_guess = input("Pick a number between 1 and 10:  ")
-    player_guess = int(player_guess)
-    if player_guess < 1 or player_guess > 10:
-        print("Please enter a number within the given range.")
-except ValueError:
-    print("Please enter a number.")
-
-print(player_guess)
+while True:
+    try:
+        player_guess = input("Pick a number between 1 and 10:  ")
+        player_guess = int(player_guess)
+        if player_guess < 1 or player_guess > 10:
+            print("Please enter a number within the given range.")
+            continue
+    except ValueError:
+        print("Please enter a number.")
+        continue
+    
+    if player_guess < answer_number:
+        print("It is higher!")
+        continue
+    elif player_guess > answer_number:
+        print("It is lower!")
+        continue
+    else:
+        print("You got it!")
+        break
