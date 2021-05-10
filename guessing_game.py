b37b984333b26158e10a51149268dcee273818c1
James-Rouse <woootlooops@gmail.com>
2021-05-10 12:36:13 -0400

    Update guessing_game.py

--- guessing_game.py
+++ guessing_game.py
@@ -1,11 +1,11 @@
 import random
 
 print("Welcome to the Number Guessing Game!")
-answer_number = random.randrange(10)
+answer_number = random.randrange(1, 11)
 
 while True:
     try:
-        player_guess = input("Pick a number between 1 and 10:  ")
+        player_guess = input("Pick a number between 1 and 10: ")
         player_guess = int(player_guess)
         if player_guess < 1 or player_guess > 10:
             print("Please enter a number within the given range.")
@@ -13,7 +13,6 @@ while True:
     except ValueError:
         print("Please enter a number.")
         continue
-    
     if player_guess < answer_number:
         print("It is higher!")
         continue
@@ -22,4 +21,21 @@ while True:
         continue
     else:
         print("You got it!")
+
+    while True:
+        play_again = input("Would you like to play again? Yes or no?: ")
+        play_again = play_again.lower()
+        if play_again == "yes":
+            break
+        elif play_again == "no":
+            break
+        else:
+            print("Please enter only yes or no!")
+            continue
+
+    if play_again == "yes":
+        answer_number = random.randrange(1, 11)
+        continue
+    else:
+        print("We hope you had fun!")
         break
